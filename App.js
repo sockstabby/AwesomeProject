0df064684fa29704c8add0ecf5debfc1092817ae
Eric Peterson <ericpeterson@Erics-MacBook-Pro.local>
2021-09-11 11:38:54 -0500

    added react-native-svg

--- App.js
+++ App.js
@@ -4,6 +4,29 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Svg, {
+	Circle,
+	Ellipse,
+	G,
+	TSpan,
+	TextPath,
+	Path,
+	Polygon,
+	Polyline,
+	Line,
+	Rect,
+	Use,
+	Image,
+	Symbol,
+	Defs,
+	LinearGradient,
+	RadialGradient,
+	Stop,
+	ClipPath,
+	Pattern,
+	Mask
+} from 'react-native-svg';
+
 function DetailsScreen() {
 	return (
 		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -16,6 +39,10 @@ function HomeScreen({ navigation }) {
 	return (
 		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 			<Text>Home screen</Text>
+			<Svg height="50%" width="50%" viewBox="0 0 100 100">
+				<Circle cx="50" cy="50" r="45" stroke="blue" strokeWidth="2.5" fill="green" />
+				<Rect x="15" y="15" width="70" height="70" stroke="red" strokeWidth="2" fill="yellow" />
+			</Svg>
 			<Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
 		</View>
 	);
